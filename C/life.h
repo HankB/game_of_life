5dcca991fc346ed7a9b595ffb14ab90a3f5a25ee
Hank Barta <hbarta@gmail.com>
2020-04-21 22:03:56 -0500

    declare struct locus and init_universe()

--- C/life.h
+++ C/life.h
@@ -2,6 +2,11 @@
 Header for functions shared between application and tests
 */
 
+typedef struct {
+    int row;
+    int col;
+} locus;
 
 bool *get_universe(unsigned int dim);
 void release_universe(bool* u); 
+int init_universe(locus coordinates[]);
