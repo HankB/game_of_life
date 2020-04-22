a92744e9a877e39afe3d9a8154ad48e2360385ae
Hank Barta <hbarta@gmail.com>
2020-04-21 22:01:30 -0500

    commentary, style

--- C/life.c
+++ C/life.c
@@ -1,8 +1,15 @@
+/**
+ * File including the main() fuction for the Game of Life implementation.
+ *
+ * Current version is more or less set aside until functionality and unit 
+ * tests are complete
+ */
 #include <stdbool.h>
 
 #include "life.h"
 
-int main( int argc, char** argv) {
+int main( int argc, char** argv)
+{
     bool * universe = get_universe(3);
     release_universe(universe);
 }
