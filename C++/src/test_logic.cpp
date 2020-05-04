23b8fe6dcc1739824cfa4dc9a820362bd155709c
Hank Barta <hbarta@gmail.com>
2020-05-03 21:11:56 -0500

    implement one unit test

--- C++/src/test_logic.cpp
+++ C++/src/test_logic.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <catch.hpp>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+#include "life.hpp"
 
-    return 0;
+int construct_destroy_cell(void) 
+{
+    Cell    c;
+    c.set_state(live);
+    return c.get_state();
+}
+
+TEST_CASE( "constructor/destructor for cell", "[]" ) {
+    REQUIRE( construct_destroy_cell() == live );
 }
